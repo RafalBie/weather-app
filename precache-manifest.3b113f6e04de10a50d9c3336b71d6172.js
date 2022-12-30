b1ebd73656f223bedbd0cd87a64fb690a678358a
id github: RafaBie <homefac@gmail.com>
2022-12-30 11:12:50 +0100

    Updates

---
+++ precache-manifest.3b113f6e04de10a50d9c3336b71d6172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b1be080332a9e40fe9bb2881b5701e3",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "5865dbef28f373bed1b5",
+    "url": "/weather-app/static/css/2.510409cd.chunk.css"
+  },
+  {
+    "revision": "d1ffd10f77671c2a62fe",
+    "url": "/weather-app/static/css/main.a3828d58.chunk.css"
+  },
+  {
+    "revision": "5865dbef28f373bed1b5",
+    "url": "/weather-app/static/js/2.c5b023d8.chunk.js"
+  },
+  {
+    "revision": "d1ffd10f77671c2a62fe",
+    "url": "/weather-app/static/js/main.3f2e298f.chunk.js"
+  },
+  {
+    "revision": "0a7b3eab350aecb16674",
+    "url": "/weather-app/static/js/runtime-main.5d70a2b7.js"
+  }
+]);
